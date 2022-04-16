00f0bdfd645f367d7631d1b8ab496e8258da9709
dhlee421 <dhlee421@gmail.com>
2022-04-16 14:14:10 +0900

    Update BinaryWriting.cs

--- BinaryWriting.cs
+++ BinaryWriting.cs
@@ -8,14 +8,14 @@ namespace BinaryWriting
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("파일이 저장될 위치를 입력하세요:");
+            Console.WriteLine("Please input location:");
             String path = Console.ReadLine();
 
             try
             {
                 FileStream fs = new FileStream(path, FileMode.Create);
 
-                // 단일 바이트 쓰기
+                // Write single byte
                 fs.WriteByte(0x01);
                 fs.WriteByte(0x23);
                 fs.WriteByte(0x45);
@@ -28,20 +28,20 @@ namespace BinaryWriting
                 Byte[] asciiByte = Encoding.ASCII.GetBytes("ASCII string to byte!");
                 Byte[] uniByte = Encoding.Unicode.GetBytes("유니코드 문자열을 바이트로 변환!");
 
-                // ASCII 바이트 쓰기
+                // Write ASCII byte
                 fs.Write(asciiByte, 0, asciiByte.Length);
 
-                // 줄바꿈 (\r\n)
+                // New line (\r\n)
                 fs.WriteByte(13);
                 fs.WriteByte(10);
 
-                // UNICODE 바이트 쓰기
+                // Write UNICODE byte
                 fs.Write(uniByte, 0, uniByte.Length);
 
-                // 위치를 처음으로!
+                // Move to beginning position 
                 fs.Position = 0;
 
-                // 단일 바이트를 총 8회 읽습니다.
+                // Read single byte 8 times.
                 for (Int32 i = 0; i < 8; i++)
                 {
                     Byte readSingleByte = (Byte)fs.ReadByte();
@@ -55,15 +55,14 @@ namespace BinaryWriting
                 fs.Read(dummyAsciiByte, 0, dummyAsciiByte.Length);
                 Console.WriteLine("읽은 ASCII 바이트: {0}", Encoding.ASCII.GetString(dummyAsciiByte));
 
-                // 줄바꿈 문자(\r\n)는 건너뛴다. 
+                // Skip new line (\r\n)
                 fs.ReadByte();
                 fs.ReadByte();
 
-                // 쓴 UNICODE 바이트를 읽고 출력해보자!
+                // Read and Print written UNICODE byte
                 fs.Read(dummyUniByte, 0, dummyUniByte.Length);
                 Console.WriteLine("읽은 UNICODE 바이트: {0}", Encoding.Unicode.GetString(dummyUniByte));
 
-                // 작업이 끝났으니 파일을 저장하고 닫는다!
                 fs.Close();
 
             }

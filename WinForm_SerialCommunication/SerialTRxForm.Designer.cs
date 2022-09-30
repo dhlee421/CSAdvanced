526f0cd53f17a9f43c692a883a2bebafe7bf7257
dhlee421 <dhlee421@gmail.com>
2022-09-30 19:03:27 +0900

    Add files via upload

---
+++ WinForm_SerialCommunication/SerialTRxForm.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace SerialCommunication
+{
+    partial class SerialTRxForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox_port = new System.Windows.Forms.ComboBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.button_disconnect = new System.Windows.Forms.Button();
+            this.textBox_send = new System.Windows.Forms.TextBox();
+            this.richTextBox_received = new System.Windows.Forms.RichTextBox();
+            this.label_send = new System.Windows.Forms.Label();
+            this.label_receive = new System.Windows.Forms.Label();
+            this.label_port = new System.Windows.Forms.Label();
+            this.button_send = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label_status = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox_port
+            // 
+            this.comboBox_port.FormattingEnabled = true;
+            this.comboBox_port.Location = new System.Drawing.Point(10, 61);
+            this.comboBox_port.Name = "comboBox_port";
+            this.comboBox_port.Size = new System.Drawing.Size(176, 21);
+            this.comboBox_port.TabIndex = 0;
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(39, 128);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(119, 48);
+            this.button_connect.TabIndex = 1;
+            this.button_connect.Text = "Connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.Button_connect_Click);
+            // 
+            // button_disconnect
+            // 
+            this.button_disconnect.Location = new System.Drawing.Point(39, 185);
+            this.button_disconnect.Name = "button_disconnect";
+            this.button_disconnect.Size = new System.Drawing.Size(119, 48);
+            this.button_disconnect.TabIndex = 1;
+            this.button_disconnect.Text = "Disconnect";
+            this.button_disconnect.UseVisualStyleBackColor = true;
+            this.button_disconnect.Click += new System.EventHandler(this.Button_disconnect_Click);
+            // 
+            // textBox_send
+            // 
+            this.textBox_send.Location = new System.Drawing.Point(258, 330);
+            this.textBox_send.Name = "textBox_send";
+            this.textBox_send.Size = new System.Drawing.Size(209, 20);
+            this.textBox_send.TabIndex = 2;
+            // 
+            // richTextBox_received
+            // 
+            this.richTextBox_received.Location = new System.Drawing.Point(258, 61);
+            this.richTextBox_received.Name = "richTextBox_received";
+            this.richTextBox_received.Size = new System.Drawing.Size(307, 217);
+            this.richTextBox_received.TabIndex = 3;
+            this.richTextBox_received.Text = "";
+            // 
+            // label_send
+            // 
+            this.label_send.AutoSize = true;
+            this.label_send.Location = new System.Drawing.Point(258, 311);
+            this.label_send.Name = "label_send";
+            this.label_send.Size = new System.Drawing.Size(58, 13);
+            this.label_send.TabIndex = 4;
+            this.label_send.Text = "Send Data";
+            // 
+            // label_receive
+            // 
+            this.label_receive.AutoSize = true;
+            this.label_receive.Location = new System.Drawing.Point(258, 41);
+            this.label_receive.Name = "label_receive";
+            this.label_receive.Size = new System.Drawing.Size(79, 13);
+            this.label_receive.TabIndex = 4;
+            this.label_receive.Text = "Received Data";
+            // 
+            // label_port
+            // 
+            this.label_port.AutoSize = true;
+            this.label_port.Location = new System.Drawing.Point(10, 41);
+            this.label_port.Name = "label_port";
+            this.label_port.Size = new System.Drawing.Size(59, 13);
+            this.label_port.TabIndex = 5;
+            this.label_port.Text = "Select Port";
+            // 
+            // button_send
+            // 
+            this.button_send.Location = new System.Drawing.Point(487, 328);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(78, 25);
+            this.button_send.TabIndex = 6;
+            this.button_send.Text = "Send";
+            this.button_send.UseVisualStyleBackColor = true;
+            this.button_send.Click += new System.EventHandler(this.Button_send_Click);
+            // 
+            // label_status
+            // 
+            this.label_status.AutoSize = true;
+            this.label_status.Location = new System.Drawing.Point(12, 278);
+            this.label_status.Name = "label_status";
+            this.label_status.Size = new System.Drawing.Size(80, 13);
+            this.label_status.TabIndex = 7;
+            this.label_status.Text = "Connect Status";
+            // 
+            // SerialTRxForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 398);
+            this.Controls.Add(this.label_status);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.label_port);
+            this.Controls.Add(this.label_receive);
+            this.Controls.Add(this.label_send);
+            this.Controls.Add(this.richTextBox_received);
+            this.Controls.Add(this.textBox_send);
+            this.Controls.Add(this.button_disconnect);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.comboBox_port);
+            this.Name = "SerialTRxForm";
+            this.Text = "Serial TRx Test Form";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_port;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.Button button_disconnect;
+        private System.Windows.Forms.TextBox textBox_send;
+        private System.Windows.Forms.RichTextBox richTextBox_received;
+        private System.Windows.Forms.Label label_send;
+        private System.Windows.Forms.Label label_receive;
+        private System.Windows.Forms.Label label_port;
+        private System.Windows.Forms.Button button_send;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label_status;
+    }
+}
+
---
+++ WinForm_SerialCommunication/SerialTRxForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace SerialCommunication
+{
+    public partial class SerialTRxForm : Form
+    {
+        public SerialTRxForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Bind Port lists to ComboBox's Data source
+            comboBox_port.DataSource = SerialPort.GetPortNames();
+        }
+
+        private void Button_connect_Click(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.PortName = comboBox_port.Text;
+                serialPort1.BaudRate = 9600;
+                serialPort1.DataBits = 8;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Parity = Parity.None;
+
+                serialPort1.RtsEnable = true;
+                serialPort1.DtrEnable = true;
+                serialPort1.ReceivedBytesThreshold = 5;
+
+                serialPort1.DataReceived += new SerialDataReceivedEventHandler(serialPort1_DataReceived);
+
+                serialPort1.Open();
+
+                label_status.Text = "Port is Open";
+                comboBox_port.Enabled = false;
+            }
+            else
+            {
+                label_status.Text = "Port is already Open";
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            this.Invoke(new EventHandler(MySerialReceived));
+        }
+
+        private void MySerialReceived(object s, EventArgs e)
+        {
+            string ReceiveData = serialPort1.ReadExisting();
+            richTextBox_received.Text += ReceiveData;
+        }
+
+        private void Button_send_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write(textBox_send.Text);
+        }
+
+        private void Button_disconnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+
+                label_status.Text = "Port is Closed";
+                comboBox_port.Enabled = true;
+            }
+            else
+            {
+                label_status.Text = "Port is already Closed";
+            }
+        }
+    }
+}
